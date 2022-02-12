b6dc1c4932c663b774b663a3047d218e9abbcaba
Raja Asyraf <rmasyraf@gmail.com>
2022-02-12 20:42:38 +0800

    Add item model and table

---
+++ database/migrations/2022_02_12_122541_create_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Inventory;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Inventory::class);
+            $table->string('name');
+            $table->double('quantity')->default(0);
+            $table->string('unit')->nullable();
+            $table->string('location')->nullable();
+            $table->timestamp('expired_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+};
