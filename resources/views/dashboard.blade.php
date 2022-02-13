66a0f33ce93b17bbdf9d82ed11ba0c4ad405ba0e
Raja Asyraf <rmasyraf@gmail.com>
2022-02-14 00:33:39 +0800

    Add bootstrap ui css & js and items table

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,28 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Quantity</th>
+                                <th scope="col">Location</th>
+                                <th scope="col">Expiry at</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($items as $item)
+                                <tr>
+                                    <th scope="row">{{ $loop->iteration }}</th>
+                                    <td>{{ $item->name }}</td>
+                                    <td>{{ $item->quantity }} {{ $item->unit }}</td>
+                                    <td>{{ $item->location }}</td>
+                                    <td>{{ $item->expired_at }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
